236c906b30d369dde0ec6e4af28e48c514e08431
Felipe Souza <systen3@gmail.com>
2021-11-16 18:59:45 -0300

    criar camida cobrinha

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ snake [0] = {
 }
 
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1 ) * box,
+    y: Math.floor(Math.random() * 15 + 1 ) * box,
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -23,6 +27,11 @@ function criarCobrinha(){
     }
 }
 
+function drawFood (){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
 document.addEventListener('keydown', update);
 
 function update (event) {
@@ -35,13 +44,14 @@ function update (event) {
 
 function iniciarJogo () {
 
-    if(snake[0].x > 16 * box && direction == "down") snake[0].x = 0;
-    if(snake[0].x < 0 && direction == "up") snake[0].x = 16 * box;
+    if(snake[0].x >= 16 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
